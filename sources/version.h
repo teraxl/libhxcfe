32d52aa4e4890d575cd427031d44286ecc50aa91
jfdelnero <jeanfrancoisdelnero@free.fr>
2015-11-11 17:26:10 +0100

    Broken TD0 support - repaired !

--- sources/version.h
+++ sources/version.h
@@ -1,7 +1,7 @@
-#define FILE_VERSION        2,8,8,1
-#define STR_FILE_VERSION   "2,8,8,1\0"
-#define STR_FILE_VERSION2  "2.8.8.1"
-#define STR_DATE           "01/22/2015\0"
+#define FILE_VERSION        2,8,9,3
+#define STR_FILE_VERSION   "2,8,9,3\0"
+#define STR_FILE_VERSION2  "2.8.9.3"
+#define STR_DATE           "09/13/2015\0"
 #define NOMFENETRE    "HxCFloppyEmulator "##STR_FILE_VERSION2
 
 #define FILEVER        FILE_VERSION
